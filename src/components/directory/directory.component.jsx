ddfa60f3c701bc8e580dc87c9449d774e31f338a
Alexandra Kolotukhina <oleksandra.kolotukhina@beetroot.se>
2021-09-30 15:26:20 +0300

    Broken our homepage into smaller components, finished styling menu-item.

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './directory.style.scss';
+import Sections from './directory.data.js';
+
+import MenuItem from '../menu-item/menu-item.component';
+
+class Directory extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            sections: Sections
+        }
+    }
+
+    render () {
+        return (
+            <div className="directory-menu">
+                { this.state.sections.map(({id, title, imageUrl, linkUrl, size}) => (
+                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                )) }
+            </div>
+        )
+    }
+}
+
+export default Directory;
